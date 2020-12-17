19245fd7a08ffe6a65f5b466fc8e75ee82dd2825
Seth Vandebrooke <sethvandebrooke@gmail.com>
2020-12-16 19:23:07 -0500

    removed unwanted semicolon

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // load javascript
-import bootstrap from './js/dependencies/bootstrap.min.js';
+import bootstrap from './js/dependencies/bootstrap.min.js'
 import Vue from './js/dependencies/vue'
 
 let app = new Vue({
